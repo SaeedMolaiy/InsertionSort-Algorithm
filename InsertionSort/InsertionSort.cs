27758348f2f33a51c182ae815b8d9f950fc6a91f
Saeed Molaiy <101869414+SaeedMolaiy@users.noreply.github.com>
2023-10-27 11:50:07 +0330

    Insertion Sort Algorithm

---
+++ InsertionSort/InsertionSort.cs
@@ -0,0 +1,26 @@
+﻿namespace InsertionSort
+{
+    internal static class InsertionSort
+    {
+        public static void InsertionSortAlgorithm(int[] arr)
+        {
+            var n = arr.Length;
+
+            for (var i = 1; i < n; i++)
+            {
+                var currentElement = arr[i];
+                var j = i - 1;
+
+                // Move elements of arr[0..i-1] that are greater than currentElement
+                // one position ahead of their current position
+                while (j >= 0 && arr[j] > currentElement)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = currentElement;
+            }
+        }
+    }
+}
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = { 12, 11, 13, 5, 6 };
+
+Console.WriteLine("Original Array:");
+PrintArray(arr);
+
+InsertionSort.InsertionSort.InsertionSortAlgorithm(arr);
+
+Console.WriteLine("\nSorted Array:");
+PrintArray(arr);
+
+static void PrintArray(IEnumerable<int> arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write(item + " ");
+    }
+    Console.WriteLine();
+}
